4a0e9e71dccd5dc3a66c0991db4b0915a14a6e9c
Aya210 <62105535+Aya210@users.noreply.github.com>
2023-05-22 23:30:39 +0300

    Update models.py

--- Shipping/models.py
+++ Shipping/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Shipping(models.Model):
+    shipping_address = models.CharField(max_length=200)
+    contact_name = models.CharField(max_length=200)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+    shipping_method = models.CharField(max_length=100)
+    shipping_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2)
+    tracking_number = models.CharField(max_length=100)
+    status_choices = [
+        ('P', 'Pending'),
+        ('S', 'Shipped'),
+        ('D', 'Delivered'),
+        ('C', 'Cancelled')
+    ]
+    status = models.CharField(max_length=1, choices=status_choices)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.status
+
